a001f5216463597cb2d2ef639d3c12bcc74bdd9a
Ander Raso Vázquez <anderraso@gmail.com>
2019-05-09 04:14:31 +0200

    initial files added
    Signed-off-by: Ander Raso Vázquez <anderraso@gmail.com>

---
+++ kanban/__main__.py
@@ -0,0 +1,274 @@
+"""Main module."""
+import urwid
+from typing import List, Tuple
+
+from kanban.board import MarkdownParser, Kanban
+
+
+# def main() -> None:
+#     """Main function."""
+#     m = MarkdownParser()
+#     kanban: Kanban = m.parse_file("taskell.md")
+#     print(str(kanban))
+#     kanban.save("otro.md")
+#
+#
+# if __name__ == '__main__':
+#     main()
+
+m = MarkdownParser()
+# kanban: Kanban = m.parse_file("taskell.md")
+kanban: Kanban = m.parse_file("kanban.md")
+
+palette = [
+    ('reveal focus', 'dark magenta', 'default', 'standout'),
+    # ('reveal focus', 'bold, dark magenta', 'default', 'standout'),
+    # ('list_title', 'dark red', 'default'),
+    ('list_title', 'dark green', 'default'),
+    # ('tag', 'black', 'dark green'),
+    ('tag', 'dark green', 'default'),
+    ('streak', 'black', 'dark red'),
+    ('card_description', 'brown', 'default'),
+    ('board_title', 'dark blue', 'default'),
+    ('footer', 'dark blue', 'black'),
+    ('footer_text', 'dark blue', 'black'),
+    ('footer_separator', 'dark magenta', 'black'),
+    ('footer_item', 'white', 'black'),
+    ('bg', 'black', 'dark blue'),]
+
+
+class SelectableText(urwid.Text):
+    def selectable(self):
+        return True
+
+    def keypress(self, size, key):
+        return key
+
+
+class PruebaEditable(urwid.Edit):
+    def __init__(self, caption=u"", edit_text=u"", multiline=False, align=urwid.LEFT, wrap=urwid.SPACE, allow_tab=False,
+                 edit_pos=None, layout=None, mask=None):
+        super().__init__(caption, edit_text, multiline, align, wrap, allow_tab, edit_pos, layout, mask)
+        self.editing = False
+
+    def selectable(self):
+        return True
+
+    def keypress(self, size, key):
+        if not self.editing:
+            if key == 'enter':
+                self.editing = True
+            elif key == 'left':
+                self.move_cursor_to_coords(size, 0, 0)
+                return super().keypress(size, key)
+            elif key == 'right':
+                self.move_cursor_to_coords(size, 7, 0)
+                return super().keypress(size, key)
+            elif key in ['down', 'right', 'up', 'left']:
+                return super().keypress(size, key)
+        else:
+            if key == 'esc':
+                self.editing = False
+            elif key in ['down', 'right', 'up', 'left']:
+                return super().keypress(size, key)
+            else:
+                return super().keypress(size, key)
+
+
+# TODO parece que va, este deberia ser la base para todo
+class UICard(urwid.LineBox):
+# class UICard(urwid.LineBox):
+    def __init__(self, text, desc: str = None, subtasks: Tuple[int] = None):
+        # TODO pila de valores
+        # tags
+        # nombre
+        # descripcion
+        # valores completados de la lista si los hay
+        self.selectable_text = SelectableText(text)
+        self.attr_map = urwid.AttrMap(self.selectable_text, '',  'reveal focus')
+
+        pile = []
+        subtitle_text = ''
+        if desc:
+            subtitle_text += '≡'
+        if subtasks:
+            subtitle_text += f' [{subtasks[0]}/{subtasks[1]}]'
+        subtitle_text = subtitle_text.strip()
+
+        self.tags = urwid.AttrMap(urwid.Text('[tag][another_tag]'), 'tag')
+        pile.append(self.attr_map)
+
+        if subtitle_text:
+            pile.append(
+                urwid.AttrMap(urwid.Text(subtitle_text), 'card_description')
+            )
+
+        self.pile = urwid.Pile(pile)
+        super().__init__(self.pile,
+                         # title='', title_align='center', tlcorner='┌', tline='─', lline='│', trcorner='┐', blcorner='╘', rline='│', bline='═', brcorner='╛'
+                        # title='', title_align='center', tlcorner='╭', tline='─', lline='│', trcorner='╮', blcorner='╰', rline='│', bline='─', brcorner='╯'
+                        title='', title_align='center', tlcorner='', tline=' ', lline='', trcorner='', blcorner='', rline='', bline='', brcorner=''
+                         )
+
+    def set_text(self, text):
+        self.selectable_text.set_text(text)
+
+
+column_list = []
+for count, col in enumerate(kanban.columns):
+    list_content = []
+    # TODO title mejor en un objeto customizado
+    list_content.append(
+        urwid.AttrMap(urwid.Text(f'{count + 1}. {col.name}'), 'list_title'),
+    )
+    for card in col.cards:
+        subtasks = None
+        if card.subtasks:
+            subtasks = (card.subtasks_completed(), len(card.subtasks))
+        list_content.append(
+            UICard(f'{card.name}', desc=card.description, subtasks=subtasks),
+        )
+    col_content = urwid.SimpleListWalker(list_content)
+    listbox = urwid.ListBox(col_content)
+    column_list.append(listbox)
+
+# cols = urwid.Columns(column_list, dividechars=4, min_width=30)
+
+class MyColumns(urwid.Columns):
+    def __init__(self, widget_list, *args, **kwargs):
+        super().__init__(widget_list, *args, **kwargs)
+
+    def keypress(self, size, key):
+        if key in ["l", "right"]:
+            super().keypress(size, 'right')
+        elif key in ["h", "left"]:
+            super().keypress(size, 'left')
+        elif key in ["k", "up"]:
+            super().keypress(size, 'up')
+        elif key in ["j", "down"]:
+            super().keypress(size, 'down')
+        else:
+            unhandled_input(key)
+
+
+cols = MyColumns(column_list, dividechars=4, min_width=30)
+
+
+filler = urwid.Filler(cols, 'top')
+
+padding = urwid.Padding(cols, left=2, right=2)
+
+
+# header = urwid.LineBox(
+#     urwid.AttrMap(urwid.Text('DAS: Proyecto final Android', align=urwid.CENTER), 'board_title')
+# )
+header = urwid.AttrMap(urwid.Text('DAS: Android Final Project\n', align=urwid.CENTER), 'board_title')
+
+# footer = urwid.LineBox(urwid.AttrMap(SelectableText('Tarjeta dos'), '',  'reveal focus'))
+footer = urwid.AttrMap(
+    urwid.Text([
+                # ('footer_text', u"Total cards: "),
+                # ('footer_item', u"11"),
+                # ('footer_separator', u" | "),
+                ('footer_text', u"Help "),
+                ('footer_item', u"?"),
+                ]),
+    'footer')
+frame = urwid.Frame(body=padding, header=header, footer=footer)
+# frame = urwid.Frame(body=padding)
+
+
+# TODO vim navigation: https://stackoverflow.com/questions/49306056/how-to-change-key-mapping-for-urwid-widget-navigation
+def unhandled_input(key):
+    # TODO controlarlo mejor en clases que lo hereden?
+    focused_list: urwid.ListBox = cols.get_focus_widgets()
+    current_card: UICard = cols.focus.focus # esto devuelve la carta
+    debug = focused_list[0]
+    col, row, *_ = cols.get_focus_path()  # devuelve el elemento, ej: [0, 2]
+    # si hay mas devuelve   ej: [0, 2, 1]
+    # puede ser interesante para coger la tarjeta y actualizarla con el nuevo valor
+    # si se guarda con el nuevo valor pasar esa tarjeta a la vista para que se actualice, o un json mejor
+    # TODO mirar si existe una forma de obtener el widget que quiero
+    item: urwid.LineBox = focused_list[1]  # line box
+    # item.set_text("Probando a meter texto")
+    # item.set_text("probando a meter texto en la carta")
+    # base = item.base_widget
+    # base.set_text("widget obtenido")
+    # TODO for debugging
+    # debug.body[1], debug.body[2] = debug.body[2], debug.body[1]
+    # debug.body.insert(1, UICard("insertado"))
+    # loop.draw_screen()
+    if key == 'enter':
+        current_card.set_text("TESTING")
+        pass
+        # card_one.set_text(repr(debug.body[1].set_text("woow")))
+    elif key == "D":  # delete card
+        # del current_card
+        if row != 0:  # don't delete de title
+            del debug.body[row]
+            loop.draw_screen()
+    elif key == "X":  # delete column
+        if len(cols.contents) > 0:
+            del cols.contents[col]
+            loop.draw_screen()
+    elif key in ["shift down", "J"]:  # delete column
+        if row < len(cols[col].body) - 1:
+            a_card = debug.body[row]  # get current card
+            del debug.body[row]
+            debug.body.insert(row + 1, a_card)
+            cols.set_focus_path([col, row + 1])
+    elif key in ["shift up", "K"]:  # delete column
+        if row > 1:  # the column title is pos 0
+            a_card = debug.body[row]  # get current card
+            del debug.body[row]
+            debug.body.insert(row - 1, a_card)
+            cols.set_focus_path([col, row - 1])
+    elif key in ["shift right", "L"]:  # delete column
+        if cols.focus_col < len(cols.contents) - 1:
+            a_card = debug.body[row]
+            cols[col + 1].body.append(a_card)
+            del debug.body[row]
+            # cols.focus_position = [1, 1]
+
+            cols.set_focus_path([col + 1, len(cols[col + 1].body) - 1])
+            # cols.set_focus_path([col + 1, len(cols[0].body)])
+
+            # listbox: urwid.ListBox = cols[col + 1]
+            # walker: urwid.ListWalker = listbox.body
+            # card = debug.body[row]
+            # cols.contents
+            # loop.draw_screen()
+    elif key in ["shift left", "H"]:  # delete column
+        if cols.focus_col != 0:
+            a_card = debug.body[row]
+            cols[col - 1].body.append(a_card)
+            del debug.body[row]
+
+            cols.set_focus_path([col - 1, len(cols[col - 1].body) - 1])
+            # card = debug.body[row]
+            # cols.contents
+            # loop.draw_screen()
+    elif key in "G$":
+        cols.set_focus_path([col, len(cols[col].body) - 1])
+    elif key in "g0^":
+        cols.set_focus_path([col, 1])
+
+    elif key in 'qQ':
+        raise urwid.ExitMainLoop()
+
+
+# screen = urwid.raw_display.Screen()
+# screen.set_terminal_properties(colors=256)
+# print(screen.colors)
+
+loop = urwid.MainLoop(frame, palette=palette, unhandled_input=unhandled_input, handle_mouse=False)
+loop.screen.set_terminal_properties(colors=256)
+
+try:
+    loop.run()
+except KeyboardInterrupt:
+    pass
+
+
+
+
---
+++ kanban/board.py
@@ -0,0 +1,193 @@
+from typing import List
+
+"""
+## To Do
+- Una tarea de prueba
+- Otra tarea
+    > Más descripción
+- Otro todo
+    * [ ] Subtareas
+## Done
+- Más tareas
+    > La descripción
+    * [ ] Una subtarea
+    * [x] Otra tarea
+    * [ ] Última tarea
+"""
+
+# TODO hardcoded parsing is not good
+HEADING = "## "
+ITEM_NAME = "- "
+ITEM_DESCRIPTION = "    > "
+SUB_ITEM = "    * "
+TODO = "    * [ ] "
+COMPLETED = "    * [x] "
+
+
+class SubTask(object):
+    """Represents a subitem"""
+    def __init__(self, name, completed=False):
+        self.name: str = name
+        self.completed: bool = completed
+
+
+class Card(object):
+    """Represents a card of a kanban board."""
+    def __init__(self, name: str, description: str = None):
+        self.name: str = name
+        self.description: str = description
+        self.subtasks: List[SubTask] = []
+
+    def append(self, subtask: SubTask):
+        """Append a new subtask"""
+        self.subtasks.append(subtask)
+
+    def __str__(self) -> str:
+        result: str = ""
+        result += ITEM_NAME + self.name
+        result += "\n"
+        if self.description:
+            result += ITEM_DESCRIPTION + self.description
+            result += "\n"
+        for subtask in self.subtasks:
+            result += TODO if subtask.completed else COMPLETED
+            result += subtask.name
+            result += "\n"
+        return result
+
+    def subtasks_completed(self) -> int:
+        """Returns the number of completed tasks"""
+        return sum(subtask.completed for subtask in self.subtasks)
+
+    def subtasks_uncompleted(self) -> int:
+        """Returns the number of completed tasks"""
+        return sum(not subtask.completed for subtask in self.subtasks)
+
+
+class CardList(object):
+    """Reperesents a card list of a kanban board."""
+    def __init__(self, name: str):
+        self.name: str = name
+        self.cards: List[Card] = []
+
+    def append(self, card: Card):
+        """Append a new card"""
+        self.cards.append(card)
+
+    def move_card_down(self, initial_pos: int) -> bool:
+        """Moves a column right"""
+        pos_right: int = initial_pos + 1
+        if pos_right >= len(self.cards):
+            return False
+        else:
+            self.cards[initial_pos], self.cards[pos_right] = self.cards[pos_right], self.cards[initial_pos]
+            return True
+
+    def move_card_up(self, initial_pos: int) -> bool:
+        """Moves a column right"""
+        pos_left: int = initial_pos - 1
+        if pos_left <= 0:
+            return False
+        else:
+            self.cards[initial_pos], self.cards[pos_left] = self.cards[pos_left], self.cards[initial_pos]
+            return True
+
+    def __str__(self) -> str:
+        result: str = ""
+        result += HEADING + self.name
+        result += "\n"
+        result += "\n"
+        for card in self.cards:
+            result += str(card)
+        return result
+
+
+class Kanban(object):
+    """Represents a kanban board."""
+    def __init__(self):
+        self.columns: List[CardList] = []
+        pass
+
+    def append(self, card_list: CardList):
+        """Append a new column"""
+        self.columns.append(card_list)
+
+    def move_column_right(self, initial_pos: int) -> bool:
+        """Moves a column right"""
+        pos_right: int = initial_pos + 1
+        if pos_right >= len(self.columns):
+            return False
+        else:
+            self.columns[initial_pos], self.columns[pos_right] = self.columns[pos_right], self.columns[initial_pos]
+            return True
+
+    def move_column_left(self, initial_pos: int) -> bool:
+        """Moves a column right"""
+        pos_left: int = initial_pos - 1
+        if pos_left <= 0:
+            return False
+        else:
+            self.columns[initial_pos], self.columns[pos_left] = self.columns[pos_left], self.columns[initial_pos]
+            return True
+
+    def save(self, path):
+        """Save Kanban contents to a file."""
+        with open(path, 'w') as f:
+            f.write(str(self))
+
+    def __str__(self) -> str:
+        result: str = ""
+        for card_list in self.columns:
+            result += str(card_list)
+            result += "\n"
+        return result
+
+
+class MarkdownParser(object):
+    """Parses markdown."""
+    def __init__(self):
+        self.kanban = None
+        self._current_card_list = None
+        self._current_card = None
+
+    def _parse_line(self, line: str):
+        """Create a kanban board by parsing lines."""
+        if line.startswith(HEADING):
+            list_title = line[len(HEADING):]
+            self._current_card_list = CardList(list_title)
+            self.kanban.append(self._current_card_list)
+            print("HEADER: ", line)
+        elif line.startswith(ITEM_NAME):
+            card_title = line[len(ITEM_NAME):]
+            self._current_card = Card(card_title)
+            self._current_card_list.append(self._current_card)
+            print("ITEM: ", line)
+        elif line.startswith(ITEM_DESCRIPTION):
+            card_description = line[len(ITEM_DESCRIPTION):]
+            self._current_card.description = card_description
+            print("DESCRIPTION: ", line)
+        elif line.startswith(SUB_ITEM):
+            subtask_name = line[len(TODO):]
+            subtask = SubTask(name=subtask_name)
+            print("SUB_ITEM: ", line)
+            if line.startswith(TODO):
+                subtask.completed = False
+                print("TODO: ", line)
+            elif line.startswith(COMPLETED):
+                subtask.completed = True
+                print("COMPLETED: ", line)
+            self._current_card.append(subtask)
+
+    def parse_file(self, path: str) -> Kanban:
+        """Parse a markdown file and return a kanban board."""
+        # Restart values
+        self.kanban = Kanban()
+        self._current_card_list = None
+        self._current_card = None
+
+        with open(path) as f:
+            text = f.read()
+            for line in text.split('\n'):
+                if line:
+                    self._parse_line(line)
+        return self.kanban
